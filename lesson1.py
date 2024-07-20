7d42d33aad06b492e92d1df1120a05f770963ac6
EdwardChang0207 <thshguitar2015@gmail.com>
2024-07-20 14:45:25 +0800

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+'''
+#Output
+#print(v1, v2, v3, ..., vn)
+#note.1 print() 可輸出很多內容，且可以是不同data type
+print('hello', 'hi', 'asda', end='')
+
+#note.2 sep 分隔符號 init:' ', end 結尾符號 init:'\n'
+print('hello', 'hi', 'asda', sep='', end='')
+
+#Input
+#input('提示字')
+#note.1 提示字不會對結果有影響
+input('pls enter ur account: \n')
+
+.: 1.的 2.對...做...
+split('分割字元') init:' '
+'''
+print(input().split())
---
+++ lesson2.py
@@ -0,0 +1,35 @@
+'''
+#variable 變數
+#variable_name = variable
+x = 10
+print(x)
+
+#Data Type 變數型態
+#1. Numbers
+整數 integer -> int: 沒有小數點 ex.-1
+浮點數 float: 有小數點 ex.3.0
+
+#2. Text
+字串 String -> str: '' or ""
+ex. input() -> str
+
+#3. Bool
+True 肯定、正確、成立
+False 否定、錯誤、不成立
+ex. condiction
+
+
+#4. List 串列 != array 陣列
+x = [10, 3.14, True, []]
+x[0]+x[1]
+
+#Opration 不同的 DataType
+int & float -> float
+int & bool -> int (except divide)
+int & str -> X
+float & bool -> float
+float & str -> X
+
+補充：
+sum([]) -> 總和
+'''
