539fe6219379d431f3d3516546517b30a2322dea
EdwardChang0207 <thshguitar2015@gmail.com>
2024-07-27 14:14:58 +0800

    lesson 5

--- lesson5.py
+++ lesson5.py
@@ -1,5 +1,6 @@
 #Format 格式化
 '''
+[法一]
 %d -> int
 %f -> float
 %s -> str
@@ -11,7 +12,6 @@ usage: '%s %d %f....' % (data1, data2, ..., dataN)
 name = 'kevin'
 height = 180
 weight = 70
-
 #姓名：＿＿＿ 身高： 體重： bmi:
 print('姓名：%7s 身高：%d 體重：%d bmi：%.2f'%(name, height, weight, weight/(height/100)**2))
 #name: kevin
@@ -21,3 +21,18 @@ weight = 70
 
 #姓名：＿＿＿ 身高： 體重： bmi:
 print('姓名：%7s 身高：%d 體重：%d bmi：%.2f'%(name, height, weight, weight/(height/100)**2))
+'''
+[法二]
+.format() -> .:1.的 2.對...做...
+順序 -> always starts from 0
+挖洞：{順序(int):至少輸出幾個字元(int)}
+如果設定至少幾N個字元:
+    -> if N > 實際輸出的字元 then 在後面補空格
+    -> else 超過就不管
+設定輸出到小數點後第Ｎ位 -> {:.Nf}
+'''
+name = 'Alanaa'
+height = 180
+weight = 70
+
+print('{1} {2} {0:5} {3:.2f}'.format(name, height, weight,weight/(height/100)**2))
